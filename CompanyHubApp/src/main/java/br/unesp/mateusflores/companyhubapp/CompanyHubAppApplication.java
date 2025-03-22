92ef277ccf70b933c03b7e45597e063bd942d0c4
mateusflores <mateusgf.42@gmail.com>
2025-03-22 19:31:10 -0300

    add backend project structure and first dependencies

---
+++ CompanyHubApp/src/main/java/br/unesp/mateusflores/companyhubapp/CompanyHubAppApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.mateusflores.companyhubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyHubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyHubAppApplication.class, args);
+    }
+
+}
---
+++ CompanyHubApp/src/test/java/br/unesp/mateusflores/companyhubapp/CompanyHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unesp.mateusflores.companyhubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyHubAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
