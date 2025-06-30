f5731323902d51c3679eb175a90268c5c15d3709
mateusflores <mateusgf.42@gmail.com>
2025-06-29 22:26:19 -0300

    now MethodArgumentNotValidException can be treated by ControllerAdvice

--- CompanyHubApp/src/main/java/br/unesp/mateusflores/companyhubapp/infra/advice/RestExceptionHandler.java
+++ CompanyHubApp/src/main/java/br/unesp/mateusflores/companyhubapp/infra/advice/RestExceptionHandler.java
@@ -5,7 +5,9 @@ import br.unesp.mateusflores.companyhubapp.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,10 +19,27 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiErrorDTO> handleResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-        log.warn("Recurso não encontrao: '{}' no caminho '{}'",
+        log.warn("Recurso não encontrado: '{}' no caminho '{}'",
                 e.getMessage(), request.getRequestURI());
 
         HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ApiErrorDTO errorDTO = new ApiErrorDTO(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(errorDTO);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        log.warn("Argumento(s) inválido(s): '{}' no caminho '{}'", e.getMessage(), request.getRequestURI());
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         ApiErrorDTO errorDTO = new ApiErrorDTO(
                 Instant.now(),
